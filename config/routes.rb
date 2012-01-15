1b0c042aa14a1eba473544b49a74cb7f00c0c39c
Clyde Simonds <mcsimonds@gmail.com>
2012-01-15 11:48:12 -0500

    adding scaffold for statuses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :statuses
+
   map.resources :stories
 
   map.resources :sections
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120115162453) do
+ActiveRecord::Schema.define(:version => 20120115164644) do
 
   create_table "assignees", :force => true do |t|
     t.integer  "user_id"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(:version => 20120115162453) do
   end
 
   create_table "sections", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "statuses", :force => true do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at"
