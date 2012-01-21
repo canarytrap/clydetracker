7cf4475c77cb6d999d2d148e88fcfc0e59811d34
Clyde Simonds <mcsimonds@gmail.com>
2012-01-21 11:06:54 -0500

    moving css/js into files

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function toggle_div(div_id)
+{
+    target = document.getElementById(div_id);
+
+    if (target.style.display == '')
+        target.style.display = 'block';
+    else
+        target.style.display = '';
+    end    
+}
