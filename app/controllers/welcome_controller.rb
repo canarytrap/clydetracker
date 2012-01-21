c73fbec0185bce0fe2097f131e8c40c590512ff1
Clyde Simonds <mcsimonds@gmail.com>
2012-01-20 21:13:11 -0500

    using a partial for backlogs section

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,12 @@
 class WelcomeController < ApplicationController
   def index
-    # TODO: move to view helper
-    @stories      = Story.find :all, :order => "id DESC"
-    @users        = User.find :all, :order => "name"
-    @statuses     = Status.find :all, :order => "id ASC"
-    @story_types  = StoryType.find :all, :order => "name ASC"
-    @sections     = Section.find :all, :order => "name ASC"
+    # TODO: get only icebox stories
+    @stories          = Story.find :all, :order => "id DESC"
+    @stories_backlog  = Story.find_all_by_section_id(2, :order => "id DESC")
+
+    @users            = User.find :all, :order => "name"
+    @statuses         = Status.find :all, :order => "id ASC"
+    @story_types      = StoryType.find :all, :order => "name ASC"
+    @sections         = Section.find :all, :order => "name ASC"
   end
 end
