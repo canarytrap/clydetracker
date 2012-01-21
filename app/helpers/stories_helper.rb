f28d046263c54a4f288964f0edae5446fe283453
Clyde Simonds <mcsimonds@gmail.com>
2012-01-21 11:56:11 -0500

    color coding stories depending on status

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,6 +1,6 @@
 module StoriesHelper
 
-  def story_icon(story)
+  def story_icon story
     if story.story_type_id == 2
       '<img src="images/tool.png" border="0" width="20">'
     elsif story.story_type_id == 3
@@ -10,4 +10,14 @@ module StoriesHelper
     end
   end
 
+  def story_color story
+    case story.status_id
+    when 2
+      return 'style="background-color:yellow;"'
+    when 3
+      return 'style="background-color:red;"'
+    when 8
+      return 'style="background-color:green;"'
+    end
+  end
 end
