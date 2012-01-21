90d98a2a7f85c409c46d1b7391d8d8501a703c0c
Clyde Simonds <mcsimonds@gmail.com>
2012-01-20 23:00:17 -0500

    creating a section for current sprint

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   def index
     @stories_icebox   = Story.find_all_by_section_id(1, :order => "id DESC")
     @stories_backlog  = Story.find_all_by_section_id(2, :order => "id DESC")
+    @stories_current  = Story.find_all_by_section_id(3, :order => "id DESC")
 
     @users            = User.find :all, :order => "name"
     @statuses         = Status.find :all, :order => "id ASC"
