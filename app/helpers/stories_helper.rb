27b8599e74636d52150a6b1ab45827ace512cc7a
Clyde Simonds <mcsimonds@gmail.com>
2012-01-21 15:51:01 -0500

    replacing bug icon

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -4,7 +4,7 @@ module StoriesHelper
     if story.story_type_id == 2
       '<img src="images/tool.png" border="0" width="20">'
     elsif story.story_type_id == 3
-      '<img src="images/bug-icon.gif" border="0" width="20">'
+      '<img src="images/bug.png" border="0" width="20">'
     else
       '<img src="images/Book-icon.png" border="0" width="20">'
     end
