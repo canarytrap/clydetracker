81aaa33e35fdfa0d8528d3a1a0658163a5ee9c57
Clyde Simonds <mcsimonds@gmail.com>
2012-02-13 20:52:52 -0500

    creating a default current sprint if none exists

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,16 +4,19 @@ class WelcomeController < ApplicationController
     @stories_icebox   = Story.find_all_by_section_id(1, :order => "id DESC")
     @stories_backlog  = Story.find_all_by_section_id(2, :order => "id DESC")
 
-    @current_sprint   = Section.current_sprint.first
+    # if it doesnt exist lets create a default one
+    Section.create_current_sprint
+
+    @current_sprint   = Section.current_sprint
     @stories_current  = Story.find_all_by_section_id(@current_sprint, :order => "id DESC")
 
-    @previous_sprint  = Section.previous_sprint.first
+    @previous_sprint  = Section.previous_sprint
     @stories_previous = Story.find_all_by_section_id(@previous_sprint, :order => "id DESC")
 
     # for select boxes
     @users            = User.find :all, :order => "name"
     @statuses         = Status.find :all, :order => "id ASC"
     @story_types      = StoryType.find :all, :order => "name ASC"
-    @sections         = Section::SELECT_BOX_SPRINTS
+    @sections         = Section.select_box
   end
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,19 +1,34 @@
 class Section < ActiveRecord::Base
-  
-  def self.current_sprint
-    find(:all, :conditions => "end_date >= curdate()", 
-          :order => "end_date asc", 
-          :limit => 1)
-  end
-  
-  def self.previous_sprint
-    find(:all, :conditions => "end_date < curdate()", 
-          :order => "end_date desc", 
-          :limit => 1)
+
+  # create a default current sprint if none exists
+  def self.create_current_sprint
+    if !self.current_sprint
+      section = self.new(:name        => "current",
+                        :description  => "default current sprint",
+                        :start_date   => Time.now,
+                        :end_date     => Time.now + (60*60*24*7))
+      section.save!
+    end
   end
 
-  # icebox and backlogs
-  SELECT_BOX_SPRINTS = [{'id' => 1, 'name' => 'Icebox'},
-                        {'id' => 2, 'name' => 'Backlog'},
-                        {'id' => self.current_sprint.first.id, 'name' => 'Current'}]
+  def self.current_sprint
+    results = find(:all, :conditions => "end_date >= curdate()", 
+                    :order => "end_date asc", 
+                    :limit => 1)
+    results.first
+  end
+
+  def self.previous_sprint
+    results = find(:all, :conditions => "end_date < curdate()", 
+                    :order => "end_date desc", 
+                    :limit => 1)
+    results.first                
+  end
+
+  def self.select_box
+    # TODO: clean this up
+    sections = [{'id' => 1, 'name' => 'Icebox'},
+                {'id' => 2, 'name' => 'Backlog'},
+                {'id' => self.current_sprint.id, 'name' => self.current_sprint.name}]
+  end
 end
