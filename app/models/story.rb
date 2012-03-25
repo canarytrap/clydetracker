57add96bff7cdbe474532e96d0069a61bf1ef9ae
Clyde Simonds <mcsimonds@gmail.com>
2012-03-24 20:18:08 -0400

    moving the setting of certain values out of controller to callback when creating story

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,12 +1,21 @@
 class Story < ActiveRecord::Base
   belongs_to :section
   has_many :assignees
-  
-  # TODO: sql injection validation
+
+  before_create :add_to_icebox
+
+# TODO: sql injection validation
   def self.search search_string
     results = find(:all, 
                     :conditions => "title LIKE '%#{search_string}%' 
                                     OR description LIKE '%#{search_string}'", 
                     :order => "created_at asc")
+  end
+
+private
+  # when creating a new story, set default values here
+  def add_to_icebox
+    self.author_id  = 1
+    self.section_id = 1
   end
 end
