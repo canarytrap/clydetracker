a131cd6c8e6a46af4f6eb4cf2bbde328a795756d
Clyde Simonds <mcsimonds@gmail.com>
2012-04-08 17:26:29 -0400

    creating a unit test for total stories helper

--- test/unit/helpers/stories_helper_test.rb
+++ test/unit/helpers/stories_helper_test.rb
@@ -1,4 +1,19 @@
 require 'test_helper'
 
 class StoriesHelperTest < ActionView::TestCase
+
+  test "should compute total points" do
+    story1 = Story.new
+    story1.points = 4
+    story2 = Story.new
+    story2.points = 3
+    # lets not give any pts for this story
+    story3 = Story.new
+
+    stories = [story1, story2, story3]
+
+    total = self.total_points stories
+
+    assert_equal 7, total
+  end
 end
