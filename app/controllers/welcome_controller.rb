a2f2b9e72f26f7c800d5021c73ff140df48bf797
Clyde Simonds <mcsimonds@gmail.com>
2012-05-20 16:00:14 -0400

    ordering stories by priority instead of id

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,13 +4,13 @@ class WelcomeController < ApplicationController
     Section.create_current_sprint
 
     # stories for each section
-    @stories_icebox   = Story.find_all_by_section_id(1, :order => "id DESC")
-    @stories_backlog  = Story.find_all_by_section_id(2, :order => "id DESC")
+    @stories_icebox   = Story.find_all_by_section_id(1, :order => "priority ASC")
+    @stories_backlog  = Story.find_all_by_section_id(2, :order => "priority ASC")
     @current_sprint   = Section.current_sprint
-    @stories_current  = Story.find_all_by_section_id(@current_sprint, :order => "id DESC")
+    @stories_current  = Story.find_all_by_section_id(@current_sprint, :order => "priority ASC")
 
     @previous_sprint  = Section.previous_sprint
-    @stories_previous = Story.find_all_by_section_id(@previous_sprint, :order => "id DESC")
+    @stories_previous = Story.find_all_by_section_id(@previous_sprint, :order => "priority ASC")
 
     # for select boxes
     @users            = User.find :all, :order => "name"
